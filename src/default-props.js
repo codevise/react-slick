06337e25f4cc33066058d1292685c240bf7e4c71
Rafael Teixeira <rteixeira@codevise.de>
2016-10-13 15:01:38 +0200

    Adjust default value for slideSpacing prop

--- src/default-props.js
+++ src/default-props.js
@@ -27,7 +27,7 @@ var defaultProps = {
     responsive: null,
     rtl: false,
     slide: 'div',
-    slideSpacing: 20,
+    slideSpacing: 0,
     slidesToShow: 1,
     slidesToScroll: 1,
     speed: 500,
